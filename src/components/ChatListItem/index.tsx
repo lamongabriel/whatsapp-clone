c485112091d8693d2323ec149980dfbf27e09b64
Gabriel Lamon <gabriel-lamon@outlook.com>
2023-03-02 17:28:36 -0300

    chatlist - styling

--- src/components/ChatListItem/index.tsx
+++ src/components/ChatListItem/index.tsx
@@ -1,3 +1,25 @@
+import Image from 'next/image';
+import styles from './styles.module.scss';
+
+import defaultAvatar from '../../assets/avatar.png';
+
 export default function ChatListItem() {
-  return <div>Chat list item</div>;
+  return (
+    <div className={styles.chatListItem}>
+      <Image
+        src={defaultAvatar}
+        className={styles.chatListAvatar}
+        alt="Avatar item"
+      />
+      <div className={styles.chatListLines}>
+        <div className={styles.chatListLine}>
+          <div>Gabriel Lamon Lopes</div>
+          <time>09:38</time>
+        </div>
+        <div className={styles.chatListLine}>
+          <p>How you doin?</p>
+        </div>
+      </div>
+    </div>
+  );
 }
