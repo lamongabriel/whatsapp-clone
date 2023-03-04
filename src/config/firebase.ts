326482d169275bd607784edf6c5a79047ed156e1
Gabriel Lamon <gabriel-lamon@outlook.com>
2023-03-04 16:10:21 -0300

    firebase basic config

---
+++ src/config/firebase.ts
@@ -0,0 +1,16 @@
+import { initializeApp } from 'firebase/app';
+import { getFirestore } from 'firebase/firestore/lite';
+import 'firebase/firebase-auth';
+import 'firebase/firebase-firestore';
+
+export const firebaseConfig = {
+  apiKey: process.env.FIREBASE_API_KEY as string,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN as string,
+  projectId: process.env.FIREBASE_PROJECT_ID as string,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET as string,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID as string,
+  appId: process.env.FIREBASE_APP_ID as string,
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
