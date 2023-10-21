0030c4054e923d3f88fb7b7fa0ce8353ec006366
Gabriel Lamon <lamongabriel@icloud.com>
2023-10-21 18:16:36 -0300

    removed semi com _app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,14 +1,14 @@
-import 'regenerator-runtime/runtime';
-import type { AppProps } from 'next/app';
+import 'regenerator-runtime/runtime'
+import type { AppProps } from 'next/app'
 
-import '../styles/global.scss';
-import { Provider } from 'react-redux';
-import store from '@/redux/store';
+import '../styles/global.scss'
+import { Provider } from 'react-redux'
+import store from '@/redux/store'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <Provider store={store}>
       <Component {...pageProps} />
     </Provider>
-  );
+  )
 }
